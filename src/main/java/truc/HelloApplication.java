66eb902f15c839c83c08e4a604fe4be0a34b9c8c
constance <knztnce@gmail.com>
2022-02-21 15:21:55 -0500

    good base for question 1

--- src/main/java/truc/HelloApplication.java
+++ src/main/java/truc/HelloApplication.java
@@ -11,5 +11,8 @@ public class HelloApplication {
         Question2 question2 = new Question2();
         Question3 question3 = new Question3();
         Question4 question4 = new Question4();
+
+        int q1 = question1.EuclidGCD(5, 5);
+        System.out.println("Euclid says the Greatest Common Divisor is : " + q1);
     }
 }
--- src/main/java/truc/Question1.java
+++ src/main/java/truc/Question1.java
@@ -1,10 +1,23 @@
 package truc;
 
+import java.util.Scanner;
+
 public class Question1 {
 
-    public Question1() {}
+    public Question1() {
+    }
 
     public int EuclidGCD(int number1, int number2) {
+        int small, big;
+        if (number1 < number2) {
+            small = number1;
+            big = number2;
+        } else if (number1 > number2) {
+            small = number2;
+            big = number1;
+        } else {
+            return number1;
+        }
         return 0;
     }
 }
