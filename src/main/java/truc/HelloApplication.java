87c0eee92b6e8ba9b8695ef874228185a36305e5
constance <knztnce@gmail.com>
2022-02-25 11:13:53 -0500

    j'avais oublié de reconfigurer le main so here it is

--- src/main/java/truc/HelloApplication.java
+++ src/main/java/truc/HelloApplication.java
@@ -10,16 +10,18 @@ public class HelloApplication {
         Question3 question3 = new Question3();
         Question4 question4 = new Question4();
 
-//        int q1 = question1.EuclidGCD(234, 7630);
-//        System.out.println("Euclid says the Greatest Common Divisor is : " + q1);
+        int q1 = question1.EuclidGCD(234, 7630);
+        System.out.println("Euclid says the Greatest Common Divisor is : " + q1 + "\n");
 
         //question2
 
-//        int q31 = question3.AddBinaries("1010111", "10100");
-//        System.out.println("The sum of 1010111 and 10100 is : " + q31);
+        int q31 = question3.AddBinaries("1010111", "10100");
+        System.out.println("87 + 20 = 107");
+        System.out.println("The sum of 1010111 and 10100 is : " + q31);
 
         int q32 = question3.AddBinaries("10011", "11010");
-        System.out.println("\nThe sum of 10011 and 11010 is : " + q32);
+        System.out.println("19 + 26 = 45");
+        System.out.println("The sum of 10011 and 11010 is : " + q32 + "\n");
 
         //question4
 
