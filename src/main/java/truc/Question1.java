d81ab866c8d93510e30d405b03524510720e012b
constance <knztnce@gmail.com>
2022-03-03 11:41:56 -0500

    added an if loop for question 1

--- src/main/java/truc/Question1.java
+++ src/main/java/truc/Question1.java
@@ -8,6 +8,10 @@ public class Question1 {
     public int EuclidGCD2(int number1, int number2) {
         int result = 0;
 
+        if (number1 == number2) {
+            return number1;
+        }
+
         while (number1 != number2) {    // This function works so that it subtracts the smaller number to the larger
             if (number1 > number2) {            // Until both number are the same
                 number1 -= number2;
